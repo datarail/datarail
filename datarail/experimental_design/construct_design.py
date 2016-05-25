bcdce7313a6153f51f162954375f2bdc191b27c3
subkar <sub.kartik@gmail.com>
2016-05-24 23:43:41 -0400

    construct_design takes objects created from cass Drug as argument

--- datarail/experimental_design/construct_design.py
+++ datarail/experimental_design/construct_design.py
@@ -1,4 +1,4 @@
-def construct_design(drugs, args):
+def construct_design(drug_list, cell_line_list, args):
     import xarray as xr
     import numpy as np
 
@@ -6,23 +6,29 @@ def construct_design(drugs, args):
     plate_rows = list(map(chr, range(65, 65+plate_dims[0])))
     plate_cols = range(1, plate_dims[1] + 1)
 
-    Designs = xr.Dataset({k: (['rows', 'cols'], np.zeros(args.plate_dims))
-                          for k in drugs.keys()},
+    
+    Designs = xr.Dataset({k.name: (['rows', 'cols'], np.zeros(plate_dims))
+                          for k in drug_list},
                          coords={'rows': plate_rows, 'cols': plate_cols})
+
+
+    # Designs = xr.Dataset({k: (['rows', 'cols'], np.zeros(args.plate_dims))
+    #                       for k in drugs.keys()},
+    #                      coords={'rows': plate_rows, 'cols': plate_cols})
 
     Designs.attrs['Seed'] = args.Seed
     Designs.attrs['well_volume'] = args.well_volume
-    Designs.attrs['plate_dims'] = args.plate_dims
+    Designs.attrs['plate_dims'] = plate_dims
 
-    for drug in drugs.keys():
-        Designs[drug].attrs['DrugName'] = drug
-        Designs[drug].attrs['Stock_conc'] = args.stock_conc
-        Designs[drug].attrs['HMSLid'] = drugs[drug]
+    for drug in drug_list:
+        Designs[drug.name].attrs['DrugName'] = drug.name
+        Designs[drug.name].attrs['Stock_conc'] = drug.stock_concentration
+        Designs[drug.name].attrs['HMSLid'] = drug.database_id
 
-    Designs['Perturbations'] = (('rows', 'cols'), np.zeros(args.plate_dims))
-    Designs['Vehicle'] = (('rows', 'cols'), np.zeros(args.plate_dims))
+    Designs['Perturbations'] = (('rows', 'cols'), np.zeros(plate_dims))
+    Designs['Vehicle'] = (('rows', 'cols'), np.zeros(plate_dims))
     Designs['treated_wells'] = (('rows', 'cols'),
-                                np.ones(args.plate_dims, dtype=bool))
+                                np.ones(plate_dims, dtype=bool))
 
     return Designs
 
