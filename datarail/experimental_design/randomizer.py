d94748a7842fb883135b8837c6ca2df6cf31eec9
subkar <sub.kartik@gmail.com>
2016-06-23 18:48:41 -0400

    refactored input to take list of drug panels

--- datarail/experimental_design/randomizer.py
+++ datarail/experimental_design/randomizer.py
@@ -1,4 +1,4 @@
-def randomizer(drug_panel, all_treatments, cntrl_pos):
+def randomizer(drug_panels, all_treatments, cntrl_pos):
     
     nwells = cntrl_pos.size   
     cntrl_idx = np.nonzero(cntrl_pos.reshape(1, nwells))[1]
@@ -6,13 +6,13 @@ def randomizer(drug_panel, all_treatments, cntrl_pos):
                                  size=nwells,
                                  replace=False)
     idx[cntrl_idx] = nwells
-    # idx_sort = [idx.index[x] for x in sorted(idx)]
     idx_sort = np.argsort(idx)
-
-    panel = drug_panel.reshape(1, nwells)
-    panel[0, idx_sort[:all_treatments.shape[1]]] = all_treatments[0, :]
-    panel = panel.reshape(drug_panel.shape)
-
+    panels = []
+    for i, panel in enumerate(drug_panels):
+        panel = drug_panel.reshape(1, nwells)
+        panel[0, idx_sort[:all_treatments.shape[1]]] = all_treatments[i, :]
+        panel = panel.reshape(drug_panel.shape)
+        panels.append(panel)
     return panel
 
           
