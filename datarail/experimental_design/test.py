3a8b82678675e517f36bb291c70e6b65ec429c76
subkar <sub.kartik@gmail.com>
2016-06-30 17:20:10 -0400

    plotting of drug layout included in test

--- datarail/experimental_design/test.py
+++ datarail/experimental_design/test.py
@@ -2,12 +2,13 @@ from construct_design import construct_design
 from treatment_df import get_df
 from parse_vararg import parse_args
 import numpy as np
+from plot_drugs import plot
 
 args = parse_args()
 drugs = ['MK2206', 'Gefitinib', 'GSK1059615',
          'BEZ235', 'Triciribine', 'PP242']
 cells = ['AU565', 'HCC1954', 'T47D', 'MCF 10A']
-n_controls = 25
+n_controls = 90
 
 drug_treatment = {}
 concentrations = np.hstack((1, 10 ** np.linspace(1, 4, 9)))
@@ -17,3 +18,4 @@ drug_treatment_df = get_df(drug_treatment, args)
 
 Designs = construct_design(drugs, cells, drug_treatment_df,
                            n_controls, args)
+plot(Designs, drugs)
