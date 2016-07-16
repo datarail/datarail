9904080098a8502eef38be33957cd6fb5118fb8f
subkar <sub.kartik@gmail.com>
2016-07-16 16:22:09 -0400

    mappng between source 96 wells plate and 3 destination 384 well plates

--- datarail/experimental_design/test_ld_compounds.py
+++ datarail/experimental_design/test_ld_compounds.py
@@ -5,6 +5,9 @@ import numpy as np
 from plot_panels import plot_drugs, plot_control_wells
 import pandas as pd
 import re
+from collections import OrderedDict
+import cPickle as pickle
+from get_well_id import get_well_id
 
 args = parse_args()
 
@@ -18,7 +21,7 @@ num_doses = 9
 num_replicates = 3
 plate_name = 'LD_20160713'
 
-drug_treatment = {}
+drug_treatment = OrderedDict()
 for drug in drugs:
     max_dose = df['Highest_Dose'].ix[
         df['Compound_Name'] == drug].values[0]
@@ -34,4 +37,44 @@ for rep in range(num_replicates):
     plot_drugs(Designs, drugs, rep)
     plot_control_wells(Designs)
 
-    
+
+with open('LD_20160713_rep_1.pkl', 'rb') as pickle_file:
+    rep1 = pickle.load(pickle_file)
+with open('LD_20160713_rep_2.pkl', 'rb') as pickle_file:
+    rep2 = pickle.load(pickle_file)
+with open('LD_20160713_rep_3.pkl', 'rb') as pickle_file:
+    rep3 = pickle.load(pickle_file)
+
+
+batch1_drugs = drugs[:8]
+batch2_drugs = drugs[8:16]
+batch3_drugs = drugs[16:24]
+batch4_drugs = drugs[24:]
+
+
+batches = [batch1_drugs, batch2_drugs,
+           batch3_drugs, batch4_drugs]
+reps = [rep1, rep2, rep3]
+
+
+def get_mapping(batch, reps, drug_treatment):
+    batch_wells = []
+    rep1_wells = []
+    rep2_wells = []
+    rep3_wells = []
+    for i, drug in enumerate(batch):
+        row = chr(65+i)
+        for j, c in enumerate(drug_treatment[drug]):
+            batch_wells.append('%s%d' % (row, j))
+            rep1_wells.append(get_well_id(reps[0], drug, c))
+            rep2_wells.append(get_well_id(reps[1], drug, c))
+            rep3_wells.append(get_well_id(reps[2], drug, c))
+    z = zip(batch_wells, rep1_wells, rep2_wells, rep3_wells)
+    df = pd.DataFrame(z, columns=['batch1', 'rep1', 'rep2', 'rep3'])
+    return df
+
+
+for i, batch in enumerate(batches):
+    df = get_mapping(batch, reps, drug_treatment)
+    filename = 'mapping_%d.csv' % (i+1)
+    df.to_csv(filename, index=False)
