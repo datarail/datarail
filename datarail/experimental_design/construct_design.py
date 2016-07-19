71d4b1e82ae36ca6429630959d9762b96b272307
Marc Hafner <hafner.marc@gmail.com>
2016-07-19 17:30:32 -0400

    edge controlled randomization

--- datarail/experimental_design/construct_design.py
+++ datarail/experimental_design/construct_design.py
@@ -6,37 +6,53 @@ import numpy as np
 import cPickle as pickle
 
 
-def construct_design(cell_lines, treatments_df,
-                     num_doses, args, barcode, random_seed=False):
+def construct_design(treatments_df, args, barcode, n_replicates=1, random_seed=True, edge_bias=True):
 
     treatments = treatments_df.keys()
+    # cleaning up the treatments_df to only have rows with a treatment
+    drug_treatment_df = treatments_df.iloc[(treatments_df.values!=0).any(axis=1),:]
+    n_treatments = len(drug_treatment_df)
     plate_dims = args.plate_dims
     plate_rows = list(map(chr, range(65, 65+plate_dims[0])))
     plate_cols = range(1, plate_dims[1] + 1)
-    Designs = xr.Dataset({k: (['rows', 'cols'], np.zeros(args.plate_dims))
+    Design1 = xr.Dataset({k: (['rows', 'cols'], np.zeros(args.plate_dims))
                           for k in treatments},
                          coords={'rows': plate_rows, 'cols': plate_cols})
 
-    Designs.attrs['Seed'] = args.Seed
-    Designs.attrs['well_volume'] = args.well_volume
-    Designs.attrs['plate_dims'] = plate_dims
-    Designs.attrs['barcode'] = barcode
+    Design1.attrs['Seed'] = args.Seed
+    Design1.attrs['well_volume'] = args.well_volume
+    Design1.attrs['plate_dims'] = plate_dims
+    # check that the length of barcode match the number of replicates
+    # Design1.attrs['barcode'] = barcode
 
     for treatment in treatments:
-        Designs[treatment].attrs['DrugName'] = treatment
-        Designs[treatment].attrs['Stock_conc'] = args.stock_conc
-        Designs[treatment].attrs['HMSLid'] = get_hmslid([treatment])[treatment]
-    Designs['Perturbations'] = (('rows', 'cols'), np.zeros(plate_dims))
-    Designs['Vehicle'] = (('rows', 'cols'), np.zeros(plate_dims))
-    Designs['treated_wells'] = (('rows', 'cols'),
-                                np.ones(plate_dims, dtype=bool))
+        Design1[treatment].attrs['DrugName'] = treatment
+        Design1[treatment].attrs['Stock_conc'] = args.stock_conc
+        Design1[treatment].attrs['HMSLid'] = get_hmslid([treatment])[treatment]
+    Design1['Perturbations'] = (('rows', 'cols'), np.zeros(plate_dims))
+    Design1['Vehicle'] = (('rows', 'cols'), np.zeros(plate_dims))
 
     n_wells = plate_dims[0] * plate_dims[1]
-    n_controls = n_wells - (len(treatments) * num_doses)
-    cntrl_pos = control_positions(plate_dims, n_controls)
-    Designs['control_wells'] = (('rows', 'cols'), cntrl_pos)
-    Designs = randomizer(treatments, Designs, treatments_df,
-                         cntrl_pos, random_seed)
-    filename = '%s.pkl' % barcode
-    pickle.dump(Designs, open(filename, 'wb'), protocol=-1)
+    n_controls = n_wells - n_treatments
+    (cntrl_pos, treated_pos) = control_positions(plate_dims, n_controls) # forced control positions
+    Design1['control_wells'] = (('rows', 'cols'), cntrl_pos)
+    Design1['treated_wells'] = (('rows', 'cols'), treated_pos)
+    Designs = randomizer(treatments, Design1, drug_treatment_df, plate_dims,
+                         cntrl_pos, n_replicates, random_seed, edge_bias)
+
+    for i in range(len(Designs)):
+        Designs[i].attrs['barcode'] = barcode[i]
+        Designs[i]['control_wells'] = (('rows', 'cols'),
+                                       reassign_cntrls(Designs[i], treatments))
+
+    for i in range(len(Designs)):
+        filename = '%s.pkl' % barcode[i]
+        pickle.dump(Designs, open(filename, 'wb'), protocol=-1)
     return Designs
+
+
+
+def reassign_cntrls(Design, treatments):
+
+    all_conc = np.array([Design[d].values for d in treatments])
+    return np.all(all_conc==0, axis=0)
