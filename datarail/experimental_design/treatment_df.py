de8c43cfc8603fee8b3528db7107276043a94254
subkar <sub.kartik@gmail.com>
2016-07-30 11:36:17 -0400

    renamed function to make_treatment_df, added docstrings

--- datarail/experimental_design/treatment_df.py
+++ datarail/experimental_design/treatment_df.py
@@ -2,16 +2,38 @@ import pandas as pd
 import numpy as np
 
 
-def get_df(drug_tr_dict, args, combo_pairs=[], combo_doses=[]):
+def make_treatment_dataframe(drug_treatment_dict, args,
+                             combo_pairs=[], combo_doses=[]):
+    """ Function that returns a long table Dataframe for
+    drug treatments with n_columns  = len(drugs) and n_rows = n_wells
+
+    Parameters
+    ----------
+    drug_treatment_dict: dict
+             dictionary of drugs as keys and the corresponding doses as values
+    args:
+       default parameters for plate_dims, stock_concentration
+    combo_pairs: list of tuples
+              list of drug combinations to be used in the experiment
+    combo_doses: dict
+             dictionary of drugs as keys and the corresponding doses used in
+             combination treatment
+
+    Returns
+    -------
+    treatment_df: pandas dataframe
+             long table dataframe where columns are drugs and rows are wells
+    """
+
     n_wells = np.dot(args.plate_dims[0], args.plate_dims[1])
-    total_treatments = len(drug_tr_dict.keys())
+    total_treatments = len(drug_treatment_dict.keys())
     all_treatments = np.zeros([total_treatments, n_wells])
     count = 0
-    drug_num = {drug: i for i, drug in enumerate(drug_tr_dict.keys())}
-    for drug in drug_tr_dict.keys():
-        n_treatments = len(drug_tr_dict[drug])
+    drug_num = {drug: i for i, drug in enumerate(drug_treatment_dict.keys())}
+    for drug in drug_treatment_dict.keys():
+        n_treatments = len(drug_treatment_dict[drug])
         all_treatments[drug_num[drug],
-                       count:count+n_treatments] = drug_tr_dict[drug]
+                       count:count+n_treatments] = drug_treatment_dict[drug]
         count += n_treatments
     for pair in combo_pairs:
         n_treatments = len(combo_doses[pair[1]])
@@ -21,6 +43,6 @@ def get_df(drug_tr_dict, args, combo_pairs=[], combo_doses=[]):
             all_treatments[drug_num[pair[1]],
                            count:count+n_treatments] = combo_doses[pair[1]]
         count += n_treatments
-    df = pd.DataFrame(all_treatments.T, columns=drug_tr_dict.keys())
-    return df
-
+    treatment_df = pd.DataFrame(all_treatments.T,
+                                columns=drug_treatment_dict.keys())
+    return treatment_df
