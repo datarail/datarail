0ece79f41eaca83cdd1549fa37049c75e8ffdadf
subkar <sub.kartik@gmail.com>
2016-07-30 10:37:59 -0400

    inlcuded function to map well to index

--- datarail/experimental_design/get_well_id.py
+++ datarail/experimental_design/get_well_id.py
@@ -12,4 +12,12 @@ def get_well_id(Designs, drug, concentration):
         for i in range(len(ind[0])):
             pos = '%s%s' % (chr(65+ind[0][i]), ind[1][i]+1)
             wells.append(pos)
-    return wells 
+    return wells
+
+
+def wellid2index(id, plate_dims):
+
+    row_num = ord(id[0]) - 65
+    col_num = int(id[1:]) - 1
+    ind = row_num*plate_dims[1] + col_num
+    return ind
