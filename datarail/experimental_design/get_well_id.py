4b7fcb2bc7688956c3a35b62ed5304a7fa7eb49c
subkar <sub.kartik@gmail.com>
2016-07-30 11:46:59 -0400

    renamed functions, added doc strings

--- datarail/experimental_design/get_well_id.py
+++ datarail/experimental_design/get_well_id.py
@@ -1,7 +1,25 @@
 import numpy as np
 
 
-def get_well_id(Designs, drug, concentration):
+def get_wells(Designs, drug, concentration):
+    """ Function that returns the well in a Design xarray
+    given the drug name and concentration
+
+    Parameters
+    ----------
+    Design: xarray structure
+
+    drug: str
+      name of drug being queried
+
+    concentration: int
+      dose value being queried
+
+    Returns
+    -------
+    wells: list
+       list of wells on the plate
+    """
     try:
         arr = Designs[drug].values
     except KeyError:
@@ -15,9 +33,24 @@ def get_well_id(Designs, drug, concentration):
     return wells
 
 
-def wellid2index(id, plate_dims):
+def get_well_index(well, plate_dims):
+    """Function that maps well cooridnate to an numerical id
+    between 0 and number of wells on the plate
 
-    row_num = ord(id[0]) - 65
-    col_num = int(id[1:]) - 1
-    ind = row_num*plate_dims[1] + col_num
-    return ind
+    Parameter
+    ---------
+    well: str
+      well coordinate on plate eg: 'B10'
+    plate_dims: np array
+       dimensions of plate
+
+    Returns
+    -------
+    index: int
+       numerical id of the well
+    """
+
+    row_num = ord(well[0]) - 65
+    col_num = int(well[1:]) - 1
+    index = row_num*plate_dims[1] + col_num
+    return index
