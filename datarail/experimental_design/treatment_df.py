74b7d70396106492eb050fab9b3328aa4af6a42b
subkar <sub.kartik@gmail.com>
2016-08-04 16:52:26 -0400

    tsv file proovided by user is read and warning regarding controls and barcode are provided, itl functions split_text and get_bndry_well_count added

--- datarail/experimental_design/treatment_df.py
+++ datarail/experimental_design/treatment_df.py
@@ -1,5 +1,107 @@
 import pandas as pd
 import numpy as np
+from collections import OrderedDict
+from itertools import groupby
+import edge_barcode
+import warnings
+
+
+def make_assay(df, plate_dims, barcode_prefix,
+               encode_plate=False, num_replicates=1):
+    df = pd.read_csv(csv_file, sep='\t')
+    drugs = df.Compound_Name[df.Role == 'treatment'].tolist()
+    positive_controls = df.Compound_Name[df.Role ==
+                                         'positive_control'].tolist()
+    negative_controls = df.Compound_Name[df.Role ==
+                                         'negative_control'].tolist()
+    barcode_treatments = df.Compound_Name[df.Role == 'barcode'].tolist()
+
+    drug_treatments = OrderedDict()
+    for drug in drugs:
+        max_dose = df['Highest_Dose'].ix[
+            df['Compound_Name'] == drug].values[0]
+        num_doses = df['num_wells'].ix[
+            df['Compound_Name'] == drug].values[0]
+        max_dose_value, _ = split_text(max_dose)
+        drug_treatments[drug] = float(max_dose_value) * 1e-4 * np.logspace(
+            0, 4, num_doses)
+
+    num_wells = plate_dims[0] * plate_dims[1]
+    num_treatments = sum(len(v) for v in drug_treatments.itervalues())
+    print ""
+    print 'This experiment intends to use %d drugs making up a' \
+        ' total of %d treatments' % (len(drugs), num_treatments)
+    num_edge_wells = get_boundary_cell_count(plate_dims)
+    inner_wells_available = num_wells - num_treatments - num_edge_wells
+
+    nc_treatments = OrderedDict()
+    for nc in negative_controls:
+        max_dose = df['Highest_Dose'].ix[
+            df['Compound_Name'] == nc].values[0]
+        num_wells = df['num_wells'].ix[
+            df['Compound_Name'] == nc].values[0]
+        max_dose_value, _ = split_text(max_dose)
+        nc_treatments[nc] = [max_dose_value] * num_wells
+    num_nc_treatments = sum(len(v) for v in nc_treatments.itervalues())
+    total_control_wells = num_nc_treatments
+
+    if num_nc_treatments < 8:
+        print ""
+        warnings.warn(
+            'Insufficent number of wells alloted for negative controls')
+        print "Atleast 8 wells have to be assigned for negative controls,"\
+            " recommended number is 12, user has currently alloted %d wells"\
+            " for negative_controls" % num_nc_treatments
+
+    pc_treatments = OrderedDict()
+    try:
+        for pc in positive_controls:
+            max_dose = df['Highest_Dose'].ix[
+                df['Compound_Name'] == pc].values[0]
+            num_wells = df['num_wells'].ix[
+                df['Compound_Name'] == pc].values[0]
+            max_dose_value, _ = split_text(max_dose)
+            pc_treatments[pc] = [max_dose_value] * num_wells
+        num_pc_treatments = sum(len(v) for v in pc_treatments.itervalues())
+        total_control_wells += num_pc_treatments
+    except NameError:
+        pass
+
+    if total_control_wells > inner_wells_available:
+        print ""
+        warnings.warn(
+            'Number of wells alloted for controls exceeds available wells')
+        print "%d wells are available for controls, user has alloted %d wells"\
+            " for negative controls and %d for positive controls" % (
+                inner_wells_available, num_nc_treatments, num_pc_treatments)
+
+    if encode_plate:
+        bc_treatments = OrderedDict()
+        try:
+            for bc in barcode_treatments:
+                max_dose = df['Highest_Dose'].ix[
+                    df['Compound_Name'] == bc].values[0]
+                num_wells = df['num_wells'].ix[
+                    df['Compound_Name'] == bc].values[0]
+                max_dose_value, _ = split_text(max_dose)
+                bc_treatments[bc] = [max_dose_value] * num_wells
+            num_bc_treatments = sum(len(v) for v in bc_treatments.itervalues())
+
+            barcodes = [barcode_predix + chr(65+i)
+                        for i in range(num_replicates)]
+            num_bc_wells = [edge_barcode.encode_barcode(bc) for bc in barcodes]
+            max_bc_wells = n.max(num_bc_wells)
+            if num_bc_treatments < max_bc_wells:
+                print ""
+                warnings.warn(
+                    'Insufficent number of wells alloted for encodng barcode')
+                print "barcode requires %d wells, user has alloted %d wells"\
+                    % (max_bc_wells, num_bc_treatments)
+        except NameError:
+            print ""
+            warnings.warn('treatments for barcode not specified')
+
+    return drug_treatments, pc_treatments, nc_treatments, bc_treatments
 
 
 def make_treatment_dataframe(drug_treatment_dict, args,
@@ -46,3 +148,30 @@ def make_treatment_dataframe(drug_treatment_dict, args,
     treatment_df = pd.DataFrame(all_treatments.T,
                                 columns=drug_treatment_dict.keys())
     return treatment_df
+
+
+def split_text(s):    
+    for k, g in groupby(s, str.isalpha):
+        yield ''.join(list(g))
+
+
+def get_boundary_cell_count(plate_dims):
+    """ get number of wells in outer or inner edges
+
+    Parameter
+    ---------
+    plate_dims: array
+         dimensions of plate
+
+    Returns
+    -------
+    boundary_cell_count: int
+           number of wells in the edges
+    """
+    boundary_cell_count = 2 * (plate_dims[0] + plate_dims[1] - 2)
+    return boundary_cell_count
+
+
+        
+
+
