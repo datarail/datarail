d17838ef65dd3f9db1a275730aea90711172b9ff
smkartik <sub.kartik@gmail.com>
2018-08-30 17:00:59 -0400

    The 1st and 2nd sigmidal fit functions were included in this commit

--- datarail/experimental_design/dr_fits.py
+++ datarail/experimental_design/dr_fits.py
@@ -26,11 +26,16 @@ def biphasic_fit_function(x, a, b, c, d, e, f):
     biphasic_function
 
     """
-    trm1 = 1 + a + (1 - a)/(1 + (x * 10 ** b) ** c)
-    trm2 = 1 + d + (1 - d)/(1 + (x * 10 ** e) ** f)
+    term1 = 1 + a + ((1 - a)/(1 + (x * 10 ** b) ** c))
+    term2 = 1 + d + ((1 - d)/(1 + (x * 10 ** e) ** f))
 
-    biphasic_function = (2 ** 0.5 * (np.log2(trm1) + np.log2(trm2))) - 1
+    biphasic_function = (2 ** 0.5 * (np.log2(term1) + np.log2(term2))) - 1
     return biphasic_function
+
+
+def sigmoidal_fit_function(x, a, b, c):
+    sigmoidal_function = a + ((1 - a)/(1 + (x * 10 ** b) ** c ))
+    return sigmoidal_function
 
 
 def fit(xdata, ydata):
@@ -47,20 +52,36 @@ def fit(xdata, ydata):
     Returns
     -------
     yfit: 1d array
-       list/array of GR values estimated based on fit
+       array of GR values estimated based on fit
     """
-    ge50_low = np.max(np.min(xdata)*1e-4, 1e-7)
-    ge50_high = np.min(np.max(xdata)*1e2, 1e2)
+    if type(xdata) == list:
+        xdata = np.array(xdata)
+    if type(ydata) == list:
+        ydata = np.array(ydata)
+    ge50_low = np.max((np.min(xdata) * 1e-4, 1e-7))
+    ge50_high = np.min((np.max(xdata) * 1e2, 1e2))
     lower_bounds = [-.05, ge50_low, .025, -1, 0.3, 0.025]
-    upper_bounds = [1, 1, 5, -.5, ge50_high, 10]
+    upper_bounds = [1, 1, 5, .5, ge50_high, 10]
 
     priors = [.1, np.median(xdata), 2, -0.1, 1, 2]
    
-    popt, pcov = curve_fit(biphasic_fit_function, xdata, ydata,
-                           bounds=(lower_bounds, upper_bounds),
-                           p0=priors)
-    yfit = biphasic_fit_function(xdata, *popt)
-    return yfit
+    popt_bp, pcov_bp = curve_fit(biphasic_fit_function, xdata, ydata,
+                                 bounds=(lower_bounds, upper_bounds),
+                                 p0=priors)
+    yfit_bp = biphasic_fit_function(xdata, *popt_bp)
+
+    popt_sig1, pcov_sig1 = curve_fit(sigmoidal_fit_function, xdata, ydata,
+                                     bounds=(lower_bounds[:3], upper_bounds[:3]),
+                                     p0=priors[:3])
+    yfit_sig1 = sigmoidal_fit_function(xdata, *popt_sig1)
+
+    popt_sig2, pcov_sig2 = curve_fit(sigmoidal_fit_function, xdata, ydata,
+                                     bounds=(lower_bounds[3:], upper_bounds[3:]),
+                                     p0=priors[3:])
+    yfit_sig2 = sigmoidal_fit_function(xdata, *popt_sig2)
+
+    
+    return yfit_bp, yfit_sig1, yfit_sig2
     
 
 
